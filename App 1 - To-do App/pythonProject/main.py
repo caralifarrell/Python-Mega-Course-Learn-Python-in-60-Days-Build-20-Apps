74f896a8ad420b41002b324a5b564a5dc91efef9
Cara-Li Farrell <carali@me.com>
2024-02-06 13:21:37 -0500

    Add files via upload

---
+++ App 1 - To-do App/pythonProject/main.py
@@ -0,0 +1,13 @@
+# App 1 - To-do App
+
+# Section 2
+# Day 1: Build a To-do App with Python (Variables, Lists, and Print)
+user_prompt = "Enter a todo: "
+todo1 = input(user_prompt)
+todo2 = input(user_prompt)
+todo3 = input(user_prompt)
+
+todos = [todo1, todo2, todo3]
+print(todos)
+
+print(type(todo1))
