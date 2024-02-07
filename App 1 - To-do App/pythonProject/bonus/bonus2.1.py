ff5857fcca60b48c825ba6d152725a10a3e70f11
Cara-Li Farrell <carali@me.com>
2024-02-07 11:31:58 -0500

    Add files via upload

---
+++ App 1 - To-do App/pythonProject/bonus/bonus2.1.py
@@ -0,0 +1,8 @@
+password = input("Enter password: ")
+
+while password != "pass123":
+    passwords = input("Enter password: ")
+
+print("Password was correct!")
+
+# Everything outside while loop is only executed once
--- App 1 - To-do App/pythonProject/main.py
+++ App 1 - To-do App/pythonProject/main.py
@@ -1,8 +1,9 @@
 # App 1 - To-do App
 
 # Section 2
-# Day 1: Build a To-do App with Python (Variables, Lists, and Print)
-user_prompt = "Enter a todo: "
+# Day 1: Variables, Lists, and Print
+'''
+user_prompt = "Enter a to-do: "
 todo1 = input(user_prompt)
 todo2 = input(user_prompt)
 todo3 = input(user_prompt)
@@ -11,3 +12,15 @@ todos = [todo1, todo2, todo3]
 print(todos)
 
 print(type(todo1))
+'''
+
+# Section 3
+# Day 2: Methods & While-Loop
+user_prompt = "Enter a to-do: "
+
+todos = []
+
+while True:
+    todo = input(user_prompt)
+    todos.append(todo) # append is a method
+    print(todos)
