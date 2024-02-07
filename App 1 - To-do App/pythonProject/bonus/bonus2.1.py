760ecc8ad1d6f666e1f9a3badf7b8328964401f7
Cara-Li Farrell <carali@me.com>
2024-02-07 11:32:47 -0500

    Delete App 1 - To-do App/pythonProject directory

--- App 1 - To-do App/pythonProject/bonus/bonus2.1.py
+++
@@ -1,8 +0,0 @@
-password = input("Enter password: ")
-
-while password != "pass123":
-    passwords = input("Enter password: ")
-
-print("Password was correct!")
-
-# Everything outside while loop is only executed once
--- App 1 - To-do App/pythonProject/experiments/e2.py
+++
@@ -1,8 +0,0 @@
-user_prompt = "Enter a to-do: "
-
-todos = []
-
-while True:
-    todo = input(user_prompt)
-    print(todo.title())
-    print(todos)
