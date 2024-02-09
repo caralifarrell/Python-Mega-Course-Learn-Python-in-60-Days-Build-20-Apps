b83cf3cbec3330c6504d155b7323d7433d62b833
Cara-Li Farrell <carali@me.com>
2024-02-09 17:33:01 -0500

    Add files via upload

--- App 1 - To-do App/main.py
+++ App 1 - To-do App/main.py
@@ -33,6 +33,7 @@ while True:
 # Section 4
 # Day 3: Match-Case and For-Loop
 
+"""
 todos = []
 
 while True:
@@ -51,5 +52,33 @@ while True:
             break
         case _:  # variable does not need to be defined, can be defined on the spot
             print("Hey, you entered an unknown command")
+
+print("Bye!")
+"""
+
+# Section 5
+# Day 4: List Indexing and Tuples
+
+todos = []
+
+while True:
+    user_action = input("Type add, show, edit or exit: ")
+    user_action = user_action.strip()
+
+    match user_action:  # matches the variable
+        case 'add':
+            todo = input("Enter a todo: ")
+            todos.append(todo)
+        case 'show':
+            for item in todos:
+                item = item.title()
+                print(item)
+        case 'edit':  # put yourself in the user's shoes when adding features
+            number = int(input("Number of the todo to edit: "))
+            number = number - 1
+            new_todo = input("Enter a new todo: ")
+            todos[number] = new_todo
+        case 'exit':
+            break
 
 print("Bye!")
