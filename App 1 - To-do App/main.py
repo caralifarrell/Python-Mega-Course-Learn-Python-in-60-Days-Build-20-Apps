3d06418013692f21d665de211163758441d8d9f6
Cara-Li Farrell <carali@me.com>
2024-02-14 12:16:26 -0500

    Add files via upload

--- App 1 - To-do App/main.py
+++ App 1 - To-do App/main.py
@@ -88,6 +88,7 @@ print("Bye!")
 # Section 6
 # Day 5: Enumeration & F-Strings
 
+"""
 todos = []
 
 while True:
@@ -100,6 +101,46 @@ while True:
             todos.append(todo)
         case 'show':
             for index, item in enumerate(todos):  # Numbering the items
+                item = item.title()
+                row = f"{index + 1}-{item}"
+                print(row)
+        case 'edit':
+            number = int(input("Number of the todo to edit: "))
+            number = number - 1
+            new_todo = input("Enter a new todo: ")
+            todos[number] = new_todo
+        case 'complete':
+            number = int(input("Number of the todo to complete: "))
+            todos.pop(number - 1)
+        case 'exit':
+            break
+
+print("Bye!")
+"""
+
+# Section 7
+# Day 6: Working with text files
+
+while True:
+    user_action = input("Type add, show, edit, complete or exit: ")
+    user_action = user_action.strip()
+
+    match user_action:
+        case 'add':
+            todo = input("Enter a todo: ") + "\n"  # So it skips lines when it records in text file
+
+            file = open('todos.txt', 'r')  # Read what's inside the text file
+            todos = file.readlines()
+            file.close()  # Closing the file object
+
+            todos.append(todo)
+
+            file = open('todos.txt', 'w')  # To store the info in a text file, file object is returned by the open
+            # function; w for write vs r for read
+            file.writelines(todos)
+            file.close()
+        case 'show':
+            for index, item in enumerate(todos):
                 item = item.title()
                 row = f"{index + 1}-{item}"
                 print(row)
